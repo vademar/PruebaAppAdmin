22483f47237b79f103b4a82dc62fe0c3a595af2a
unknown <amigo_diablito@hotmail.com>
2018-11-13 12:01:44 -0400

    listo Lector de QR basico

--- app/src/main/java/com/example/valdemar/admevent/Myqr.java
+++ app/src/main/java/com/example/valdemar/admevent/Myqr.java
@@ -1,13 +1,50 @@
 package com.example.valdemar.admevent;
 
+import android.app.DatePickerDialog;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class Myqr extends AppCompatActivity {
+import com.google.zxing.Result;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.RequestParams;
 
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class Myqr extends AppCompatActivity implements ZXingScannerView.ResultHandler{
+
+    private ZXingScannerView escannerView;
+    private TextView LECTURA;
+    private String Dato;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_myqr);
+    }
+
+    public void EscanerQR(View view){
+        escannerView =new ZXingScannerView(this);
+        setContentView(escannerView);
+        escannerView.setResultHandler(this);
+        escannerView.startCamera();
+    }
+
+    @Override
+    public void handleResult(Result result) {
+        Dato = result.getText();
+        setContentView(R.layout.activity_myqr);
+        escannerView.stopCamera();
+        LECTURA= (TextView)findViewById(R.id.LECTURA);
+        LECTURA.setText(Dato);
+        control();
+    }
+
+    private void control() {
+        //REGISTRAR AL USUARIO EN UN EVENTO PREVIAMENTE REGISTRADO
+        Toast.makeText(this,"User: "+Dato,Toast.LENGTH_LONG).show();
     }
 }
